cea0be45063f8c5f2f428d4fceb3c9aaef3761cd
binhnguyen <leechiupin@gmail.com>
2023-10-21 12:36:00 +0900

    Tạm thời tạo giỏ hàng

--- BTShoeShop/src/BTShoe/BTShoe.jsx
+++ BTShoeShop/src/BTShoe/BTShoe.jsx
@@ -9,6 +9,7 @@ import data from "./data.json"
 export const BTShoe = () => {
     
     const [productDetail, setProductdetail] = useState({
+        // state default nếu ko có sự thay đổi state
         id: 1,
         name: 'Adidas Prophere',
         alias: 'adidas-prophere',
@@ -24,11 +25,41 @@ export const BTShoe = () => {
     const handleProductDetail = (product) => {
         setProductdetail(product)
     }
+
+    const [carts, setCarts] = useState({
+        id: 1,
+            name: 'Adidas Prophere',
+            alias: 'adidas-prophere',
+            price: 350,
+            description:
+                'The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n',
+            shortDescription:
+                'The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n',
+            quantity: 995, // số tượng hàng tồn kho
+            image: 'http://svcy3.myclass.vn/images/adidas-prophere.png',
+            // cái này do ban đầu ko có trong data nên mình phải tự tạo
+            cartQuantity: 1, // số lượng mua hàng
+    })
+
+    const handleCarts = (product) => {
+        setCarts(product)
+    }
     return (
         <div className="container mt-3">
             <div className="d-flex justify-content-between">
                 <h1>Shoe Shop</h1>
-
+                <p 
+                    className="fs-3"
+                    style={{
+                        // khi chỉ chuột vào hiện ra bàn tay
+                        cursor: "pointer",
+                    }}
+                    data-bs-toggle="modal"
+                    data-bs-target="#cartModal"
+                >
+                    Giỏ hàng
+                    <i className="fa-solid fa-cart-plus me-2"></i>
+                </p>
 
             </div>
             {/* do setSate đang nằm bên ProductItem nên mình phải truyền hàm handleProductDetail xuống cho ProductItem cho nó setState, gián tiếp qua component ProductList, tất nhiên bên ProductList phải truyền thêm lần nữa mới qua đc ProdcuctItem */}
@@ -41,7 +72,9 @@ export const BTShoe = () => {
             <ProductDetailModal 
                 productDetail={productDetail}
             />
-            <CartModal />
+            <CartModal 
+                carts={carts}
+            />
 
         </div>
     )
--- BTShoeShop/src/BTShoe/CartModal.jsx
+++ BTShoeShop/src/BTShoe/CartModal.jsx
@@ -1,9 +1,106 @@
 // import React from 'react'
 
-export const CartModal = () => {
+export const CartModal = (props) => {
+  const {carts} = props
   return (
-    <div>
-        
+    <div
+      className="modal fade"
+      id="cartModal"
+      tabIndex={-1}
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog modal-xl">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="exampleModalLabel">
+              Cart
+            </h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            />
+          </div>
+          <div className="modal-body">
+            {/* nếu carts rỗng */}
+            {!carts.length && <h2>Vui lòng chọn sản phẩm</h2>}
+            {/* nếu carts có sản phẩm */}
+            {!!carts.length && (
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>STT</th>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Descripttion</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {carts.map((value, index) => {
+                    // map thì phải có return
+                    return (
+                      // map thì phải truyền key vào
+                      <tr key={value.id}>
+                        <td>{index + 1}</td>
+                        <td>
+                          <img
+                            style={{
+                              width: 120,
+                            }}
+                            src={value.image}
+                            alt="..."
+                          />
+                        </td>
+                        <td>{value.name}</td>
+                        <td>
+                          <p
+                            style={{
+                              maxWidth: 300,
+                            }}
+                          >
+                            {value.description}
+                          </p>
+                        </td>
+                        <td>{value.price}</td>
+                        <td>
+                          {/* <button
+                            className="btn btn-outline-success"
+                            onClick={() => handleCartQuantity(value.id, 1)}
+                          >
+                            +
+                          </button>
+                          <span className="mx-2">{value.cartQuantity}</span>
+                          <button
+                            className="btn btn-outline-danger"
+                            onClick={() => handleCartQuantity(value.id, -1)}
+                          >
+                            -
+                          </button> */}
+                        </td>
+                        <td>{value.cartQuantity * value.price}</td>
+                        <td>
+                          {/* <button
+                            className="btn btn-outline-dark"
+                            onClick={() => deleteCart(value.id)}
+                          >
+                            X
+                          </button> */}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
