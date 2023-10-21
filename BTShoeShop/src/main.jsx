09a1e26d6c56d1507f9aabb07dd9abbfea83e520
binhnguyen <leechiupin@gmail.com>
2023-10-21 21:08:22 +0900

    Bỏ StrictMode tránh lỗi ấn 1 nhưng bị thêm 2 sp vào giỏ hàng

--- BTShoeShop/src/main.jsx
+++ BTShoeShop/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // bỏ StrictMode ngay đây ko thì khi thêm vào giỏ hàng nó chạy code 2 lần
+  // bấm 1 lần nhưng món hàng nhưng bị thêm vào 2
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <App />
+);
